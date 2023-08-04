42b694dde9d4f49d82510a9d1cb2e220733e38cc
yeounsu0331 <yeounsu0331@gmail.com>
2023-08-04 21:17:19 +0900

    게임설명 삭제, 게임시작 위치 수정

--- main.py
+++ main.py
@@ -3,37 +3,25 @@ import pygame
 import sys
 from env import * # 설정값 저장 파일: env.py
 from func import * # 게임 기능 함수 파일: func.py
-from screen.game_desc import * # 게임 설명 파일: screen\game_desc.py
 from screen.game_start import * # 게임 실행 파일: screen\game_start.py
 
 ###### 초기화 ######
 init() # Pygame 초기화
-screen = pygame.display.set_mode((screen_width, screen_height)) # 화면 크기 설정
-
-# 커서 초기 위치: 게임 시작
-cursor_x = start_cursor_x
-cursor_y = start_cursor_y
-
-# 텍스트 설정
-selected_text = None
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # 화면 크기 설정
 
 # 플래그 설정
 screen_flag = 1 # 1: 메인 화면, 2: 게임 설명 화면, 3: 게임 시작 화면
-text_flag = 0 # 0: 선택 X, 1: 게임 시작, 2: 게임 설명
 
 ###### 게임 화면 ######
 while True:
     # 마우스 위치 설정
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    
     if screen_flag == 1: # 메인 화면 일 때
-        screen.fill(white)  # 흰색 배경화면
+        screen.fill(WHITE)  # 흰색 배경화면
         
         # 출력
-        show_text("2023 Pygame Competition", Title_font, black, screen, screen_width // 2, screen_height // 2) # 타이틀 출력
-        show_text("게임 시작", Body_font, black, screen, screen_width // 2 - 80, screen_height - 100) # 게임 시작 출력
-        show_text("게임 설명", Body_font, black, screen, screen_width // 2 + 80, screen_height - 100) # 게임 설명 출력
-        show_text("[ 원하는  메뉴를  클릭하세요 ]", main_desc_font, black, screen, screen_width // 2, screen_height - 50) # 설명 출력
+        show_text("2023 Pygame Competition", TITLE_FONT, BLACK, screen, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) # 타이틀 출력
+        show_text("[ 게임 시작 ]", BODY_FONT, BLACK, screen, SCREEN_WIDTH // 2 , START_Y) # 게임 시작 출력
 
         pygame.display.update() # 화면 업데이트
     
@@ -45,14 +33,10 @@ while True:
             
             elif event.type == pygame.MOUSEBUTTONDOWN: # 마우스 눌렀을 때
                 if event.button == 1: # 마우스 왼쪽 버튼 눌렀을 때
-                    if mouse_x >= 600 and mouse_x <= 680 and mouse_y >= 610 and mouse_y <= 630: # 마우스가 게임 시작 위치에 있을 때
+                    if mouse_x >= 560 and mouse_x <= 720 and mouse_y >= 520 and mouse_y <= 550: # 마우스가 게임 시작 위치에 있을 때
                         screen_flag = 2 # 게임 시작 화면으로 이동
-                    elif mouse_x >= 600 and mouse_x <= 680 and mouse_y >= 610 and mouse_y <= 630: # 마우스가 게임 설명 위치에 있을 때
-                        screen_flag = 3 # 게임 설명 화면으로 이동
                     else:
                         screen_flag = 1 # 메인 화면으로 이동
 
-    elif screen_flag == 2: # 게임 설명 화면 일 때
+    elif screen_flag == 2: # 게임 시작을 눌렀을 때
         game_start(screen) # 게임 시작 함수 호출
-    elif screen_flag == 3:
-        game_desc(screen) # 게임 설명 함수 호출
